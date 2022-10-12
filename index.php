88801fcbdb4061c703e669e627e119e5ec0e9888
Magnus Felinto <magnusfelinto@gmail.com>
2022-10-12 00:41:27 -0300

    blaa

--- index.php
+++ index.php
@@ -7,9 +7,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/estilo/index.css" media="screen" />
     <title>Nosso site</title>
+    <style type="text/css">
+    </style>
+    <script type="text/javascript" src=""></script>
 </head>
-<body>
-   
+<body >
+<div class="contain-to-grid">
+      <nav class="top-bar" data-topbar="">
+        <ul class="title-area">
+          <li class="name">
+            <h1><a href="/dashboard/index.html"></a></h1>
+          </li>
+          <li class="toggle-topbar menu-icon">
+            <a href="#">
+              <span>Menu</span>
+            </a>
+          </li>
+        </ul>
+        <section class="top-bar-section">
+          <ul class="right">
+              <li class=""><a href="/applications.html">Applications</a></li>
+              <li class=""><a href="/dashboard/faq.html">FAQs</a></li>
+              <li class=""><a href="/dashboard/howto.html">HOW-TO Guides</a></li>
+              <li class=""><a target="_blank" href="/dashboard/phpinfo.php">PHPInfo</a></li>
+              <li class=""><a href="/phpmyadmin/">phpMyAdmin</a></li>
+          </ul>
+        </section>
+      </nav>
+    </div>
 </body>
 </html>
